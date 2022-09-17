4b30c88cdd75d7b6c419ba98a0a6eab366dc38ba
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2022-09-17 09:42:49 -0300

    refactoring everything

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,18 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type IconProps = {
+  size?: number,
+}
+
+interface InputProps {
+  name: string;
+  icon?: IconProps;
+  placeholder: string;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -37,7 +47,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {/* {Icon && <Icon size={20} />} */}
 
       <input
         onFocus={handleInputFocus}
